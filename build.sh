576f4ce41751d17f23470fdb72c78c995468aa9d
jasan <jasi7234@colorado.edu>
2022-04-08 23:31:33 -0600

    include gstreamer plugins to the build

--- build.sh
+++ build.sh
@@ -59,5 +59,71 @@ else
 fi
 
 
+#add firmware support and wifi details, add camera related package support
+IMAGE_ADD="IMAGE_INSTALL:append = \"	v4l-utils python3 ntp wpa-supplicant 
+							  fbida fbgrab ffmpeg imagemagick gstreamer1.0 
+            				  gstreamer1.0-plugins-good gstreamer1.0-plugins-base  
+            				  gstreamer1.0-plugins-ugly gstreamer1.0-libav gst-player
+            				  gstreamer1.0-meta-base gst-examples gstreamer1.0-rtsp-server\""
+
+cat conf/local.conf | grep "${IMAGE_ADD}" > /dev/null
+local_imgadd_info=$?
+
+if [ $local_imgadd_info -ne 0 ];then
+    echo "Append ${IMAGE_ADD} in the local.conf file"
+	echo ${IMAGE_ADD} >> conf/local.conf
+else
+	echo "${IMAGE_ADD} already exists in the local.conf file"
+fi
+
+
+#Licence
+LICENCE="LICENSE_FLAGS_WHITELIST = \"commercial\""
+#this is required so that gstreamer1.0-plugins-ugly can be added to the image
+LICENSE:append = " commercial_gstreamer1.0-plugins-ugly commercial_mpg123"
+
+cat conf/local.conf | grep "${LICENCE}" > /dev/null
+local_licn_info=$?
+
+if [ $local_licn_info -ne 0 ];then
+    echo "Append ${LICENCE} in the local.conf file"
+	echo ${LICENCE} >> conf/local.conf
+else
+	echo "${LICENCE} already exists in the local.conf file"
+fi
+
+#Add meta-oe from openembedded
+bitbake-layers show-layers | grep "meta-oe" > /dev/null
+layer_oe_info=$?
+
+if [ $layer_oe_info -ne 0 ];then
+	echo "Adding meta-oe layer"
+	bitbake-layers add-layer ../meta-openembedded/meta-oe
+else
+	echo "meta-oe layer already exists"
+fi
+
+#Add meta-python from openembedded
+bitbake-layers show-layers | grep "meta-python" > /dev/null
+layer_python_info=$?
+
+if [ $layer_python_info -ne 0 ];then
+	echo "Adding meta-python layer"
+	bitbake-layers add-layer ../meta-openembedded/meta-python
+else
+	echo "meta-python layer already exists"
+fi
+
+#Add meta-networking from openembedded
+bitbake-layers show-layers | grep "meta-networking" > /dev/null
+layer_networking_info=$?
+
+if [ $layer_networking_info -ne 0 ];then
+	echo "Adding meta-networking layer"
+	bitbake-layers add-layer ../meta-openembedded/meta-networking
+else
+	echo "meta-networking layer already exists"
+fi
+
 set -e
 bitbake core-image-aesd
